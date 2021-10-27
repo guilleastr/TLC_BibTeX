56da462d14bc58a5e9ac556d5884d51264a790db
uo269450 <UO269450@uniovi.es>
2021-10-27 18:30:42 +0100

    Modification of html indexation Creating a base template on a txt

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import re
-
+import html_indexer as htmli
 PATH = ''
 file = 'exemplo-utf8.bib'
 
@@ -14,54 +14,14 @@ def applyER_text(ER,file):
         f.close()
     return matches
 
-def count_matches(matches):
-    dic = {}
-    for match in matches:
-        try:
-            dic[match.group()] += 1
-        except:
-            dic[match.group()] = 1
-    
-    return dic
 
-def convert2HTML(dic):
-    s = '<UL>\n'
-    tags = [x for x in dic]
-    for tag in tags:
-        s += f'\t<LI>{tag[1:]}: {dic[tag]}</LI>\n'
-    s += '</UL>'
-    return s
 
-def convert2HTMLchave(dic):
-    s = '<UL>\n'
-    tags = [x for x in dic]
-    for tag in tags:
-        s += f'\t<LI>{tag[1:-2]}: {dic[tag]}</LI>\n'
-    s += '</UL>'
-    return s
+htmli.write_to_file(applyER_text(r'@[a-zA-Z]+',PATH+file), "1.html")
+htmli.write_to_file(applyER_text(r'\{[a-zA-Z0-9.:\-\\]+,\n',PATH+file),"2.html")
 
-html = '''<!DOCTYPE html>
-<html>
-<head>
-<title>TLC 1</title>
-</head>
-<body>
 
-<h1>Categorias do BibTeX</h1>
-'''
-html += convert2HTML(count_matches(applyER_text(r'@[a-zA-Z]+',PATH+file)))
-html += convert2HTMLchave(count_matches(applyER_text(r'\{[a-zA-Z0-9.:\-\\]+,\n',PATH+file)))
 
-#Esta expressão regular encontra todos os autores mas é preciso remover todos os espaços repetidos
-#para depois poder fazer tag[n:]
-#Há uma frase no ficheiro .bib que está em camps lock por isso talvez tenhamos de tornar esta ER case insensitive
-#html += convert2HTMLchave(count_matches(applyER_text(r'author[ \t]*=[ \t]*[{"][^}"]+[\n\t ]*[}"]',PATH+file)))
 
-#É preciso fazer a mesma coisa 
-#html += convert2HTMLchave(count_matches(applyER_text(r' title[ \t]*=[ \t]*[{"][^}"]+[\n\t ]*[}"]',PATH+file)))
 
-html += '</body>\n</html>'
 
-with open('index.html','w') as f:
-    f.write(html)
-    f.close()
+
---
+++ html_indexer.py
@@ -0,0 +1,78 @@
+
+base_file="html_base.txt"
+
+###Using the "@" as a separator, when the for comes to it's line writes all the data 
+def write_file(text, filename):
+    c=open(base_file,"r")
+    html=""
+    for line in c.readlines():
+        if(line.__contains__("@")):
+            html+=text
+        else:    
+            html+=str(line)    
+
+    with open(filename,'w') as f:
+        f.write(html)
+        f.close()
+    c.close()
+
+def convert2HTML(dic):
+    s = '<UL>\n'
+    tags = [x for x in dic]
+    for tag in tags:
+        s += f'\t<LI>{tag[1:]}: {dic[tag]}</LI>\n'
+    s += '</UL>'
+    return s
+
+##deprecated
+def convert2HTMLchave(dic):
+    s = '<UL>\n'
+    tags = [x for x in dic]
+    for tag in tags:
+        s += f'\t<LI>{tag[1:-2]}: {dic[tag]}</LI>\n'
+    s += '</UL>'
+    return s
+    
+def count_matches(matches):
+    dic = {}
+    for match in matches:
+        ### Easier to clean the dic in advance, so we dont need two functions
+        tag=str(match.group()).replace("{","").replace(",","").replace("\n", "")
+        try:
+            dic[tag] += 1
+        except:
+            dic[tag] = 1
+    
+    return dic
+
+def write_to_file(matches,file):
+    text= convert2HTML(count_matches(matches))
+    write_file(text,file)
+
+def parse_search(matches,file):
+    return convert2HTML(count_matches(matches))
+
+def parse_document(doc):
+    authors=""
+    i=0
+    for auth in doc.authors: 
+        i+=1
+        authors += auth 
+        if(i<len(doc.authors)>1):
+            authors+=","
+
+    s="<strong>"+str(doc.title)+"</strong>"
+    s+="<p> Autores:"+str(authors)+" </p>"
+    s+="<p> Categoría:"+str(doc.category)  +" </p>"
+    s+="<p> Clave:"+str(doc.key)+" </p>"
+
+
+def write_document(documents,file):
+    text=""
+    for document in documents:
+        text += parse_document(document)
+    write_file(text,file)
+
+
+
+
